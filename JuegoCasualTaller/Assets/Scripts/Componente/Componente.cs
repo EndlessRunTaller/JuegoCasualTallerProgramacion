df096d3b38c9c139a3952b8731f6e4c4e389c2fa
DouglasVargas123 <d.vargasvsquez1@uandresbello.edu>
2023-05-08 22:30:47 -0400

    Cambio 17

--- JuegoCasualTaller/Assets/Scripts/Componente/Componente.cs
+++ JuegoCasualTaller/Assets/Scripts/Componente/Componente.cs
@@ -9,8 +9,23 @@ public class Componente : MonoBehaviour
     public PlayerMovementAI playerMovementAI;
     public GameObject[] cajas;
     public GameObject[] gabinetes;
+
     public float dinero;
     public TextMeshProUGUI dineroTexto;
+
+    private int nivelGabinete;
+    private int nivelCPU;
+    private int nivelRAM;
+
+    private int valorGabinete;
+    private int valorCPU;
+    private int valorRAM;
+
+    public GameObject[] flechaMejora;
+    public GameObject[] subMenu;
+
+
+
 
 
     // Start is called before the first frame update
@@ -24,5 +39,73 @@ public class Componente : MonoBehaviour
     {
         dinero = dinero + 0 * Time.deltaTime;
         dineroTexto.text = "" + Mathf.Round(dinero);
+        GABINETEMEJORA();
+        CPUMEJORA();
+        RAMMEJORA();
+
+
+    }
+
+    //MEJORAS
+    void GABINETEMEJORA()
+    {
+        if (dinero > valorGabinete)
+        {
+            flechaMejora[0].SetActive(true);
+        }
+        else
+        {
+            flechaMejora[0].SetActive(false);
+        }
+    }
+    void CPUMEJORA()
+    {
+        if (dinero > valorCPU)
+        {
+            flechaMejora[1].SetActive(true);
+        }
+        else
+        {
+            flechaMejora[1].SetActive(false);
+        }
+    }
+    void RAMMEJORA()
+    {
+        if (dinero > valorRAM)
+        {
+            flechaMejora[2].SetActive(true);
+        }
+        else
+        {
+            flechaMejora[2].SetActive(false);
+        }
+    }
+
+
+    //OTRO MENÚ
+
+    public void SUBMENUGABINETE()
+    {
+        if(subMenu[0] != null)
+        {
+            bool IsActive = subMenu[0].activeSelf;
+            subMenu[0].SetActive(!IsActive);
+        }
+    }
+    public void SUBMENUCPU()
+    {
+        if (subMenu[1] != null)
+        {
+            bool IsActive = subMenu[1].activeSelf;
+            subMenu[1].SetActive(!IsActive);
+        }
+    }
+    public void SUBMENURAM()
+    {
+        if (subMenu[2] != null)
+        {
+            bool IsActive = subMenu[2].activeSelf;
+            subMenu[2].SetActive(!IsActive);
+        }
     }
 }
